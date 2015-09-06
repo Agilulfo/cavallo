2f81130167b98583b1ecd66d40ee1d790c81f858
corsair <dinataleflavio@gmail.com>
2015-09-06 21:48:45 +0200

    try to use the idea of friend board

--- cavallo.py
+++ cavallo.py
@@ -1,14 +1,12 @@
 import random
 import time
 
+
 def padded_number(number, length):
     string_repr = str(number)
     for x in range(length - len(string_repr)):
         string_repr = ' ' + string_repr
     return string_repr
-
-def AlreadyVisited(Exception):
-    pass
 
 
 def numeral_to_an(x, y):
@@ -35,16 +33,39 @@ def get_moves(x, y):
     ]
     return [move for move in moves if is_valid_point(*move)]
 
+
 def random_point():
     return random.randint(0, 7), random.randint(0, 7)
 
 
 class ChessBoard:
+    board_size = 8
+
     def __init__(self):
         self.board = [[0 for row in range(8)] for column in range(8)]
+        self.friend_board = [[0 for row in range(8)] for column in range(8)]
+        self.calculate_friend_board()
         self.sequence = []
         self.step = 0
-        self.board_size = 8
+
+    def calculate_friend_board(self):
+        for x in range(self.board_size):
+            for y in range(self.board_size):
+                self.friend_board[x][y] = len(self.get_valid_moves(x, y))
+
+    def add_friend_board(self, x, y):
+        self.friend_board[x][y] = -1
+        influenced = get_moves(x, y)
+        for x, y in influenced:
+            if self.friend_board[x][y] != -1:
+                self.friend_board[x][y] -= 1
+
+    def remove_friend_board(self, x, y):
+        self.friend_board[x][y] = len(self.get_valid_moves(x, y))
+        influenced = get_moves(x, y)
+        for x, y in influenced:
+            if self.friend_board[x][y] != -1:
+                self.friend_board[x][y] += 1
 
     def is_visited(self, x, y):
         return self.board[x][y] != 0
@@ -55,19 +76,38 @@ class ChessBoard:
         self.step += 1
         self.board[x][y] = self.step
         self.sequence.append((x, y))
-        
+        self.add_friend_board(x, y)
+
     def get_last_move(self):
         return self.sequence[-1]
 
-    def get_next_moves(self):
-        start_point = self.get_last_move()
-        moves = get_moves(*start_point)
+    def get_valid_moves(self, x, y):
+        moves = get_moves(x, y)
         return self.filter_valid_moves(moves)
+
+    def get_next_moves(self):
+        if not self.worth_to_proceed():
+            return []
+        start_point = self.get_last_move()
+        return self.get_valid_moves(*start_point)
+
+    def worth_to_proceed(self):
+        ones = 0
+        for column in self.friend_board:
+            for value in column:
+                if value == 0:
+                    return False
+                if value == 1:
+                    ones +=1
+                    if ones == 2:
+                        return False
+        return True
 
     def undo_last_move(self):
         x, y = self.sequence.pop()
         self.board[x][y] = 0
         self.step -= 1
+        self.remove_friend_board(x, y)
 
     def filter_valid_moves(self, moves):
         return [move for move in moves if not self.is_visited(*move)]
@@ -101,10 +141,10 @@ class ChessBoard:
             line += '   {} '
         return line.format(*letters)
 
-    def __str__(self):
+    def repr_board(self, board):
         string = ''
         for row in reversed(range(self.board_size)):
-            values = [padded_number(self.board[x][row], 2) for x in range(self.board_size)]
+            values = [padded_number(board[x][row], 2) for x in range(self.board_size)]
             string += self.orizontal_line() + '\n'
             #string += self.empty_line() + '\n'
             string += self.numbered_line(row + 1).format(*values) + '\n'
@@ -113,6 +153,11 @@ class ChessBoard:
         string += self.alfa_line()
         return string
 
+    def print_friends(self):
+        print self.repr_board(self.friend_board)
+
+    def __str__(self):
+        return self.repr_board(self.board)
 
 def solve_horse_problem(first_step=None):
     first_step = first_step or random_point()
@@ -124,7 +169,8 @@ def solve_horse_problem(first_step=None):
 
 def solve(chessboard):
     #print chessboard
-    #time.sleep(0.7)
+    #chessboard.print_friends()
+    #time.sleep(0.2)
     if chessboard.step == 8 * 8:
         return chessboard
     next_moves = chessboard.get_next_moves()
