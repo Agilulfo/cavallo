f3f9aebf1fbdf4ec7eb68e3701134572851b0e60
corsair <dinataleflavio@gmail.com>
2015-09-06 20:31:54 +0200

    Add __str__ to ChessBoard

--- cavallo.py
+++ cavallo.py
@@ -1,5 +1,11 @@
 import random
+import time
 
+def padded_number(number, length):
+    string_repr = str(number)
+    for x in range(length - len(string_repr)):
+        string_repr = ' ' + string_repr
+    return string_repr
 
 def AlreadyVisited(Exception):
     pass
@@ -38,6 +44,7 @@ class ChessBoard:
         self.board = [[0 for row in range(8)] for column in range(8)]
         self.sequence = []
         self.step = 0
+        self.board_size = 8
 
     def is_visited(self, x, y):
         return self.board[x][y] != 0
@@ -65,6 +72,47 @@ class ChessBoard:
     def filter_valid_moves(self, moves):
         return [move for move in moves if not self.is_visited(*move)]
 
+    def orizontal_line(self):
+        line = '   '
+        for column in range(self.board_size):
+            line += '+----'
+        line += '+'
+        return line
+
+    def empty_line(self):
+        line = '   '
+        for column in range(self.board_size):
+            line += '|    '
+        line += '|'
+        return line
+
+    def numbered_line(self, number):
+        line = '{} '.format(padded_number(number, 2))
+        for column in range(self.board_size):
+            line += '| {} '
+        line += '|'
+        return line
+
+    def alfa_line(self):
+        a = ord('a')
+        letters = [chr(l) for l in range(a, a + self.board_size)]
+        line = '   '
+        for column in range(self.board_size):
+            line += '   {} '
+        return line.format(*letters)
+
+    def __str__(self):
+        string = ''
+        for row in reversed(range(self.board_size)):
+            values = [padded_number(self.board[x][row], 2) for x in range(self.board_size)]
+            string += self.orizontal_line() + '\n'
+            #string += self.empty_line() + '\n'
+            string += self.numbered_line(row + 1).format(*values) + '\n'
+            #string += self.empty_line() + '\n'
+        string += self.orizontal_line() + '\n'
+        string += self.alfa_line()
+        return string
+
 
 def solve_horse_problem(first_step=None):
     first_step = first_step or random_point()
@@ -75,6 +123,8 @@ def solve_horse_problem(first_step=None):
 
 
 def solve(chessboard):
+    #print chessboard
+    #time.sleep(0.7)
     if chessboard.step == 8 * 8:
         return chessboard
     next_moves = chessboard.get_next_moves()
@@ -89,7 +139,7 @@ def solve(chessboard):
 
 def main():
     chessboard = solve_horse_problem()
-    print chessboard.board
+    print chessboard
 
 if __name__=="__main__":
     main()
