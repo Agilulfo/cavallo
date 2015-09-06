0379c04aa61147a564270dbe11b4a1f5f6f6f01c
corsair <dinataleflavio@gmail.com>
2015-09-06 09:14:09 +0200

    attempt first solution (not working)

--- cavallo.py
+++ cavallo.py
@@ -1,3 +1,4 @@
+import random
 
 
 def AlreadyVisited(Exception):
@@ -21,12 +22,15 @@ def is_valid_point(x, y):
 
 def get_moves(x, y):
     moves = [
-        (x+1, y+2), (x+2, y+1),
-        (x+2, y-1), (x+1, y-2),
-        (x-1, y-2), (x-2, y-1),
-        (x-2, y+1), (x-1, y+2)
+        (x + 1, y + 2), (x + 2, y + 1),
+        (x + 2, y - 1), (x + 1, y - 2),
+        (x - 1, y - 2), (x - 2, y - 1),
+        (x - 2, y + 1), (x - 1, y + 2)
     ]
     return [move for move in moves if is_valid_point(*move)]
+
+def random_point():
+    return random.randint(0, 7), random.randint(0, 7)
 
 
 class ChessBoard:
@@ -39,11 +43,19 @@ class ChessBoard:
         return self.board[x][y] != 0
 
     def add_move(self, x, y):
-        if self.is_visited(x, y)
+        if self.is_visited(x, y):
             raise AlreadyVisited
         self.step += 1
         self.board[x][y] = self.step
         self.sequence.append((x, y))
+        
+    def get_last_move(self):
+        return self.sequence[-1]
+
+    def get_next_moves(self):
+        start_point = self.get_last_move()
+        moves = get_moves(*start_point)
+        return self.filter_valid_moves(moves)
 
     def undo_last_move(self):
         x, y = self.sequence.pop()
@@ -54,12 +66,30 @@ class ChessBoard:
         return [move for move in moves if not self.is_visited(*move)]
 
 
-def main():
-    horse = Horse(2, 0)
-    moves = horse.get_moves()
-    an_moves = [numeral_to_an(*move) for move in moves]
-    print an_moves
+def solve_horse_problem(first_step=None):
+    first_step = first_step or random_point()
+    chessboard = ChessBoard()
+    chessboard.add_move(*first_step)
+    next_move = first_step or random_point()
+    return solve(chessboard)
 
+
+def solve(chessboard):
+    if chessboard.step == 8 * 8:
+        return chessboard
+    next_moves = chessboard.get_next_moves()
+    for move in next_moves:
+        chessboard.add_move(*move)
+        solution = solve(chessboard)
+        if solution:
+            return solution
+    chessboard.undo_last_move()
+    return None
+
+
+def main():
+    chessboard = solve_horse_problem()
+    print chessboard.board
 
 if __name__=="__main__":
     main()
